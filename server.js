5066b98ddab0261594c17d686e39486bd23d75b2
Nesha Whit <neshawhitmire@gmail.com>
2022-09-21 20:33:00 -0400

    heroku

--- server.js
+++ server.js
@@ -52,28 +52,28 @@ app.post('/api/notes', (req, res) => {
 // );
 
 
-app.delete("/api/notes/:id", function (req, res) {
-    let jsonFilePath = path.join(__dirname, "/db/db.json");
-    // request to delete note by id.
-    for (let i = 0; i < database.length; i++) {
+// app.delete("/api/notes/:id", function (req, res) {
+//     let jsonFilePath = path.join(__dirname, "/db/db.json");
+//     // request to delete note by id.
+//     for (let i = 0; i < database.length; i++) {
 
-        if (database[i].id == req.params.id) {
-            // Splice takes i position, and then deletes the 1 note.
-            database.splice(i, 1);
-            break;
-        }
-    }
-    // Write the db.json file again.
-    fs.writeFileSync(jsonFilePath, JSON.stringify(database), function (err) {
+//         if (database[i].id == req.params.id) {
+//             // Splice takes i position, and then deletes the 1 note.
+//             database.splice(i, 1);
+//             break;
+//         }
+//     }
+//     // Write the db.json file again.
+//     fs.writeFileSync(jsonFilePath, JSON.stringify(database), function (err) {
 
-        if (err) {
-            return console.log(err);
-        } else {
-            console.log("Your note was deleted!");
-        }
-    });
-    res.json(database);
-});
+//         if (err) {
+//             return console.log(err);
+//         } else {
+//             console.log("Your note was deleted!");
+//         }
+//     });
+//     res.json(database);
+// });
 
 
 
